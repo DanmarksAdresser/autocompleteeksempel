6ac69735c7198846075f05ac0826a406845a9cca
finnjordal <finn@jordal.dk>
2019-09-03 11:15:29 +0200

    Forenkling

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 import './styles.css';
 import autocomplete from 'autocomplete.js';
+import 'babel-polyfill';
+import 'whatwg-fetch';
 
 function initAutocomplete(input, url, selected) {
 
   function search(url) {
-    return function search(query, callback) {
-      fetch(url+query)
-      .catch(function (error) {
-        alert(error.message);
-        callback([]);
-      })
-      .then(function(response) {
-        if (response.status >=400 && response.status <= 499) {
-          response.json().then(function (object) {
-            alert(object.type + ': ' + object.title);
-          });            
-          callback([]);
-        }
-        else if (response.status >= 200 && response.status <=299 ){
-          return response.json();
-        }
-      }) 
-      .then( function ( resultat ) { 
-        callback(resultat);
-      });
+    return async function search(query, callback) {
+      let response= await fetch(url+query);
+      let resultat= await response.json();
+      callback(resultat);
     }
   }
 
-  autocomplete(input, { debug: true, hint: false, templates: { empty: '' }, autoselect: true }, [
+  autocomplete(input, {}, [
     {
       source: search(url),
       displayKey: 'betegnelse',
@@ -35,21 +21,15 @@ function initAutocomplete(input, url, selected) {
         suggestion: function(suggestion) {
           return '<div>' + suggestion.betegnelse + '</div>';
         }
-      }
+     }
     }
   ]).on('autocomplete:selected', function(even, suggestion, dataset) {
-    even, dataset;
-   // console.log('selected', suggestion, dataset);
     selected(suggestion);
-  }).on('autocomplete:cursorchanged', function(even, suggestion, dataset) {
-    even, suggestion, dataset;
-    //console.log('cursorchanged', suggestion, dataset);
   });
 }
 
 function selected(objekt) {
 	document.getElementById('label').textContent = 'Du valgte ' + objekt.betegnelse;
-	//alert('Du har valgt ' + objekt.betegnelse);
 }
 
-initAutocomplete('#input', 'https://dawa.aws.dk/stednavne2?autocomplete&q=', selected);
+initAutocomplete('#input', 'https://dawa.aws.dk/jordstykker?autocomplete&q=', selected);
